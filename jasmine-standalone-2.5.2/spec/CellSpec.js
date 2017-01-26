b01b63a79183b92c02b56f58e6f388a030281a06
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-26 10:26:33 +0000

    adding methods to change the contents of cell to Nought or Cross

--- jasmine-standalone-2.5.2/spec/CellSpec.js
+++ jasmine-standalone-2.5.2/spec/CellSpec.js
@@ -13,7 +13,19 @@
     })
 
     it("should be initialised with empty content", function() {
-      expect(cell.content).toEqual("empty");
+      expect(cell.contents).toEqual("Empty");
     });
+
+    describe("during a game", function() {
+      it("selecting a nought changes the contents property of the cell", function() {
+        cell.makeNought();
+        expect(cell.contents).toEqual("Nought");
+      })
+
+      it("selecting a cross changes the contents property of the cell", function() {
+        cell.makeCross();
+        expect(cell.contents).toEqual("Cross");
+      })
+    })
   });
 })();
--- jasmine-standalone-2.5.2/src/Cell.js
+++ jasmine-standalone-2.5.2/src/Cell.js
@@ -3,7 +3,15 @@
   function Cell(xPosition, yPosition) {
     this.xPosition = xPosition;
     this.yPosition = yPosition;
-    this.content = "empty";
+    this.contents = "Empty";
+  }
+
+  Cell.prototype.makeNought = function() {
+    this.contents = "Nought";
+  }
+
+  Cell.prototype.makeCross = function() {
+    this.contents = "Cross";
   }
 
   exports.Cell = Cell;
